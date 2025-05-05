43dfade2d746e48fc86771adb71eb14edf90d4a1
Adaías Magdiel <adaiasmagdiell@gmail.com>
2025-05-05 01:23:43 -0300

    tests: introduce new model classes for User and Post Added User model with fields for id, name, email, active status, and creation timestamp. Added Post model with fields for id, user ID, title, and content. Defined relationship between Post and User models using the belongsTo method.

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,7 @@
 <?php
 
+use AdaiasMagdiel\Rubik\Model;
+
 uses()->beforeEach(function () {
     // Reset database connection before each test
     if (AdaiasMagdiel\Rubik\DatabaseConnection::isConnected()) {
@@ -13,3 +15,35 @@ uses()->afterAll(function () {
         Mockery::close();
     }
 });
+
+class User extends Model
+{
+    protected static function fields(): array
+    {
+        return [
+            'id' => self::Int(autoincrement: true, primaryKey: true),
+            'name' => self::Text(notNull: true),
+            'email' => self::Text(unique: true, notNull: true),
+            'active' => self::Boolean(default: true),
+            'created_at' => self::DateTime(default: 'CURRENT_TIMESTAMP'),
+        ];
+    }
+}
+
+class Post extends Model
+{
+    protected static function fields(): array
+    {
+        return [
+            'id' => self::Int(autoincrement: true, primaryKey: true),
+            'user_id' => self::Int(notNull: true),
+            'title' => self::Text(notNull: true),
+            'content' => self::Text(),
+        ];
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
