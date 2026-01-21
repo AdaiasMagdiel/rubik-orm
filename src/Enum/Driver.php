4bbf9508f01e5541289144880c155fe1e3178e5c
Adaías Magdiel <adaiasmagdiell@gmail.com>
2026-01-21 19:42:54 -0300

    style: add documentation to Driver enum

--- src/Enum/Driver.php
+++ src/Enum/Driver.php
@@ -2,8 +2,23 @@
 
 namespace AdaiasMagdiel\Rubik\Enum;
 
+/**
+ * Enumerates the supported database drivers for the Rubik ORM.
+ *
+ * This enum is used to specify which database engine the Rubik class should
+ * connect to and configure (e.g., during Rubik::connect()).
+ *
+ * @package AdaiasMagdiel\Rubik\Enum
+ */
 enum Driver: string
 {
+	/**
+	 * SQLite database driver.
+	 */
 	case SQLITE = 'SQLITE';
+
+	/**
+	 * MySQL or MariaDB database driver.
+	 */
 	case MYSQL = 'MYSQL';
 }
